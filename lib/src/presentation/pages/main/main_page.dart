ce4d4364f321fb6a983854e9c948240de5405dc1
“ocean-Soft287” <ocean.soft.kw@gmail.com>
2026-03-31 16:16:32 +0200

    feat: Update Firebase configuration and app settings

--- lib/src/presentation/pages/main/main_page.dart
+++ lib/src/presentation/pages/main/main_page.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'dart:io';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
